cf6e51f6ba2a39500d3c3ecbeffb44b3fa09fb1c
Nick O <nicholas.oetinger@gmail.com>
2019-09-13 13:38:52 -0400

    Fixed issue. Had folder misspelled.

---
+++ client/build/precache-manifest.333714a67ab13854448cddf885cf291f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dd78695c147a923d2a5",
+    "url": "/static/css/main.fb44d5a9.chunk.css"
+  },
+  {
+    "revision": "2dd78695c147a923d2a5",
+    "url": "/static/js/main.2dd78695.chunk.js"
+  },
+  {
+    "revision": "9fcb96d19fee9da6234f",
+    "url": "/static/js/1.9fcb96d1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b86ce009fb41c7875da1e185ed24755",
+    "url": "/index.html"
+  }
+];
