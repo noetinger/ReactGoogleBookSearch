e70b69a25caa20dff2bca72e830a207b2dcff97c
Nick O <nicholas.oetinger@gmail.com>
2019-09-13 11:34:03 -0400

    Reworked server.js

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require('./routes');
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// Define Middleware Here -------------------------------------
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Serve Up Static Assets (usually on Heroku) -----------------
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Define API Routes Here -------------------------------------
+app.use(routes);
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+// Connect to the MongoDB Database ----------------------------
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/googlebooks';
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+app.listen(PORT, () => {
+  console.log(`API Server now on port: ${PORT}`)
 });
